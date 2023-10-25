c815eaf0a686df9026bcccddd142743046fdf4b7
kadequilla <kadequilla@newbrighton.edu.ph>
2023-10-25 17:46:13 +0800

    added inventory user table

---
+++ public/nvt_user.sql
@@ -0,0 +1,16 @@
+create table nvt_user
+(
+    nvt_user_id bigserial
+        primary key,
+    firstname   varchar(30),
+    middlename  varchar(30),
+    lastname    varchar(30),
+    username    varchar(30) not null
+        unique,
+    password    varchar(30) not null,
+    datecreated timestamp with time zone
+);
+
+alter table nvt_user
+    owner to postgres;
+
