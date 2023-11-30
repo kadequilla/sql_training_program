cc6cc23783ce05030c660f0b1607f878afb164db
kadequilla <kadequilla@newbrighton.edu.ph>
2023-11-30 15:26:58 +0800

    feat: update view module

--- public/view_all_module.sql
+++ public/view_all_module.sql
@@ -1,9 +1,8 @@
-CREATE VIEW view_all_module(module_id, module_code, module_name, stub_curno, date_created) AS
+CREATE VIEW view_all_module(module_id, module_code, module_name, date_created) AS
 SELECT module_id,
-       module_code,
-       module_name,
-       stub_curno,
-       date_created
+       UPPER(module_code::TEXT)                  AS module_code,
+       INITCAP(module_name::TEXT)                AS module_name,
+       TO_CHAR(date_created, 'YYYY-MM-DD'::TEXT) AS date_created
 FROM module;
 
 ALTER TABLE view_all_module
